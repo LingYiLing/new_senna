8289b8d3005e84fe569c7ab45950fe5e32571688
Tab_Locke <2723733484@qq.com>
2020-09-03 12:20:50 +0000

    测试忽略已提交的的文件

--- hello.c
+++ hello.c
@@ -3,7 +3,6 @@
 int main(void)
 {
 	printf("Hello, world!\n");
-	printf("ILYFIKaros!\n");	
 
 	return 0;
 }
--- test/test.c
+++ test/test.c
@@ -3,7 +3,7 @@
 int main()
 {
 	printf("ILYFIKaros\n");
-
+	printf("Hello World\n");
 
 
 	return 0;
